aeb70b26324f3b78c13dc975fdddc83cd0f025fe
Riken <rikengala@gmail.com>
2020-05-13 22:12:17 +0530

    Updates

---
+++ precache-manifest.e0382aba676b1305cf972902c756ec28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3870a9903fdf20452e28b57bf6700c63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da915c4830c7ca4fa39a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "eeef1f0f5f4a26d0bc0f",
+    "url": "/static/js/2.d15918b4.chunk.js"
+  },
+  {
+    "revision": "443d2105bbae44cc4ccf3384197799b0",
+    "url": "/static/js/2.d15918b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da915c4830c7ca4fa39a",
+    "url": "/static/js/main.e8ac2cd3.chunk.js"
+  },
+  {
+    "revision": "9fd2eb78645235536c20",
+    "url": "/static/js/runtime-main.8252708c.js"
+  }
+]);
