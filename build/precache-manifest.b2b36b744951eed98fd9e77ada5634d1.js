55d776a2c3e5ed2cc0846f1268014f621978f153
Riken <rikengala@gmail.com>
2021-09-30 20:46:40 -0400

    Production build

---
+++ build/precache-manifest.b2b36b744951eed98fd9e77ada5634d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1120da06d2edec2eab94ce189e013400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30f66fd45629b79ad433",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1c8e63403a21dccc73fd",
+    "url": "/static/js/2.04f1320b.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/static/js/2.04f1320b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f66fd45629b79ad433",
+    "url": "/static/js/main.6f59cf9f.chunk.js"
+  },
+  {
+    "revision": "e066af11374f9903671a",
+    "url": "/static/js/runtime-main.982ba2df.js"
+  }
+]);
